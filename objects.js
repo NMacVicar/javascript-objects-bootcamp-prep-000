b300705f9db6abf5534c81515ee647344f2bb18e
Neil MacVicar <n.macvicar9@gmail.com>
2017-11-14 16:26:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,12 @@
 var playlist = new Object({Jasmine Rodgers: 'Two Years'})
+
+function updatePlaylist(playlist, artist, songTitle){
+  playlist.artist = 'songTitle'
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist.artist
+  return playlist
+}
+
